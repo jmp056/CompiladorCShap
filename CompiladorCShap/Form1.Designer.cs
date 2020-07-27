fbcb8167d040b5c672cf2da06e604ec065ef7962
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-07-27 19:18:00 -0400

    Componentes de la ventana listos

---
+++ CompiladorCShap/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace CompiladorCShap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CodigoTextBox = new System.Windows.Forms.TextBox();
+            this.ResultadoTextBox = new System.Windows.Forms.TextBox();
+            this.CompilarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CodigoTextBox
+            // 
+            this.CodigoTextBox.Location = new System.Drawing.Point(10, 10);
+            this.CodigoTextBox.Multiline = true;
+            this.CodigoTextBox.Name = "CodigoTextBox";
+            this.CodigoTextBox.Size = new System.Drawing.Size(600, 350);
+            this.CodigoTextBox.TabIndex = 0;
+            // 
+            // ResultadoTextBox
+            // 
+            this.ResultadoTextBox.Location = new System.Drawing.Point(70, 370);
+            this.ResultadoTextBox.Multiline = true;
+            this.ResultadoTextBox.Name = "ResultadoTextBox";
+            this.ResultadoTextBox.Size = new System.Drawing.Size(540, 40);
+            this.ResultadoTextBox.TabIndex = 1;
+            // 
+            // CompilarButton
+            // 
+            this.CompilarButton.Location = new System.Drawing.Point(10, 369);
+            this.CompilarButton.Name = "CompilarButton";
+            this.CompilarButton.Size = new System.Drawing.Size(40, 40);
+            this.CompilarButton.TabIndex = 2;
+            this.CompilarButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 423);
+            this.Controls.Add(this.CompilarButton);
+            this.Controls.Add(this.ResultadoTextBox);
+            this.Controls.Add(this.CodigoTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CodigoTextBox;
+        private System.Windows.Forms.TextBox ResultadoTextBox;
+        private System.Windows.Forms.Button CompilarButton;
+    }
+}
+
